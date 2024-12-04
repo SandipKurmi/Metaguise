228aec32bed07e977800e03a9bac7fc595865b65
Sana Saeed <ss.uitech01@gmail.com>
2024-12-04 14:43:33 +0500

    added swipper

---
+++ src/components/Swiper.jsx
@@ -0,0 +1,57 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import './Swipper.css'; // Custom styles
+
+export const Swipper = () => {
+  return (
+    <Swiper
+      spaceBetween={0}
+      slidesPerView={1}
+      autoplay={{
+        delay: 3000,
+        disableOnInteraction: false,
+      }}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Pagination, Autoplay]}
+      style={{ height: '100vh' }} // Full-height slider
+    >
+      <SwiperSlide>
+        <div
+          className='slide'
+          style={{ backgroundImage: `url('../assets/banner1.png')` }}
+        >
+          <div className='slide-content'>
+            <h1>Experience Timeless Architecture</h1>
+            <p>that Redefine Sophistication</p>
+          </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div
+          className='slide'
+          style={{ backgroundImage: `url('../assets/banner2.png')` }}
+        >
+          <div className='slide-content'>
+            <h1>Illuminating Architectural Brilliance</h1>
+            <p>Facades that Captivate</p>
+          </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div
+          className='slide'
+          style={{ backgroundImage: `url('../assets/banner3.png')` }}
+        >
+          <div className='slide-content'>
+            <h1>Reimagining Interiors</h1>
+            <p>with Distinct Aesthetics</p>
+          </div>
+        </div>
+      </SwiperSlide>
+    </Swiper>
+  );
+};
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,20 +3,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import MainContent from '../../components/MainContent';
 import Footer from '../../components/Footer';
 import Metavision from '../../components/Metavision';
-import Cofee  from '../../components/Cofee';
+import Cofee from '../../components/Cofee';
 import Features from '../../components/Features';
 import Product from '../../components/Product';
 import Brochure from '../../components/Brochure';
 import Team from '../../components/Team';
-import Metaland  from '../../components/Metaland';
-
-
-
+import Metaland from '../../components/Metaland';
+import { Swipper } from '../../components/Swiper';
 
 function Home() {
   return (
-    <div className="Home">
-      <MainContent />
+    <div className='Home'>
+      <Swipper />
+      {/* <MainContent /> */}
       <Features />
       <Metavision />
       <Product />
