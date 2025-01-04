9ab3600f92b58cc1e40fbfdcbea67debe40cb66b
Vrushali-Pansare <vrushalipansare508@gmail.com>
2025-01-04 15:43:50 +0530

    refactor Header component for improved layout, responsiveness, and mobile support

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,51 +1,83 @@
-import React, { useState, useEffect } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faFacebook,
   faInstagram,
   faYoutube,
-} from '@fortawesome/free-brands-svg-icons';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
-import './Header.css';
-import logo from '../assets/logo.png';
-import Drawer from './Drawer';
+} from "@fortawesome/free-brands-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import "./Header.css";
+import logo from "../assets/logo.png";
+import Drawer from "./Drawer";
 
 function Header() {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
 
   // Toggle drawer visibility
   const toggleDrawer = () => {
     setIsDrawerOpen((prevState) => !prevState);
   };
 
+  // Handle window resize
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   // Disable body scroll when the drawer is open
   useEffect(() => {
-    document.body.style.overflow = isDrawerOpen ? 'hidden' : 'auto';
+    document.body.style.overflow = isDrawerOpen ? "hidden" : "auto";
     return () => {
-      document.body.style.overflow = 'auto';
+      document.body.style.overflow = "auto";
     };
   }, [isDrawerOpen]);
 
   return (
     <div>
       <Drawer isOpen={isDrawerOpen} toggleDrawer={toggleDrawer} />
-      <header className='header'>
-        <div className='header-left'>
-          <div className='social-icons'>
-            <a href='https://gmail.com/' className='icon'>
-              <FontAwesomeIcon icon={faFacebook} />
-            </a>
-            <a href='https://gmail.com/' className='icon'>
-              <FontAwesomeIcon icon={faInstagram} />
-            </a>
-            <a href='https://gmail.com/' className='icon'>
-              <FontAwesomeIcon icon={faYoutube} />
-            </a>
+      <header className="header">
+        {!isMobile && (
+          <div className="header-left">
+            <div className="social-icons">
+              <a
+                href="https://facebook.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="icon"
+              >
+                <FontAwesomeIcon icon={faFacebook} />
+              </a>
+              <a
+                href="https://instagram.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="icon"
+              >
+                <FontAwesomeIcon icon={faInstagram} />
+              </a>
+              <a
+                href="https://youtube.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="icon"
+              >
+                <FontAwesomeIcon icon={faYoutube} />
+              </a>
+            </div>
           </div>
+        )}
+
+        <div className="header-center">
+          <img src={logo} alt="Metaguise Logo" className="logo" />
         </div>
-        <img src={logo} alt='Logo' className='logo' />
-        <div className='header-right'>
-          <div className='menu'>
+
+        <div className="header-right">
+          <div className="menu">
             <FontAwesomeIcon icon={faBars} onClick={toggleDrawer} />
           </div>
         </div>
