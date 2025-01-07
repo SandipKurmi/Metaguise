cfa4f654eadaf67f34abb6e281b4d23a780276b1
Vrushali-Pansare <vrushalipansare508@gmail.com>
2025-01-07 18:40:15 +0530

    refactor Featured component; improve code readability and consistency in styling

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,17 +1,17 @@
-import { Container } from 'react-bootstrap';
-import './Features.css';
-import Featured1 from '../assets/Featured/f1.png';
-import Featured2 from '../assets/Featured/f2.png';
-import Featured3 from '../assets/Featured/f3.png';
-import Featured4 from '../assets/Featured/f4.png';
-import Featured5 from '../assets/Featured/f5.png';
-import Featured6 from '../assets/Featured/f6.png';
-import Featured7 from '../assets/Featured/f7.png';
-import Featured8 from '../assets/Featured/f8.png';
+import { Container } from "react-bootstrap";
+import "./Features.css";
+import Featured1 from "../assets/Featured/f1.png";
+import Featured2 from "../assets/Featured/f2.png";
+import Featured3 from "../assets/Featured/f3.png";
+import Featured4 from "../assets/Featured/f4.png";
+import Featured5 from "../assets/Featured/f5.png";
+import Featured6 from "../assets/Featured/f6.png";
+import Featured7 from "../assets/Featured/f7.png";
+import Featured8 from "../assets/Featured/f8.png";
 import { MdArrowOutward } from "react-icons/md";
-import gsap from 'gsap';
-import { useGSAP } from '@gsap/react';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 const Features = () => {
   const featuredImages = [
@@ -28,23 +28,23 @@ const Features = () => {
   gsap.registerPlugin(useGSAP);
 
   useGSAP(() => {
-    const imagesDiv = document.querySelector('.featured-row');
+    const imagesDiv = document.querySelector(".featured-row");
     let imagesDivWidth = 2064;
     let amountToScroll = imagesDivWidth - window.innerWidth;
 
-    console.log(imagesDiv, imagesDivWidth, amountToScroll);
-    amountToScroll += 80
+    // console.log(imagesDiv, imagesDivWidth, amountToScroll);
+    amountToScroll += 80;
 
     const tween = gsap.to(imagesDiv, {
       x: -amountToScroll,
       duration: 3,
-      ease: 'none',
+      ease: "none",
     });
 
     ScrollTrigger.create({
-      trigger: '.featured-section',
-      start: 'top 20%',
-      end: '+=' + amountToScroll,
+      trigger: ".featured-section",
+      start: "top 20%",
+      end: "+=" + amountToScroll,
       pin: true,
       animation: tween,
       scrub: 1,
@@ -53,32 +53,32 @@ const Features = () => {
 
   return (
     <>
-      <div className='featured-section'>
-        <Container className='featured-projects-section text-center'>
-          <div className='featured-text'>Featured Projects</div>
-          <div className='featured-row-wrapper'>
+      <div className="featured-section">
+        <div className="featured-projects-section text-center">
+          <div className="featured-text">Featured Projects</div>
+          <div className="featured-row-wrapper">
             <div
-              className='featured-row'
+              className="featured-row"
               style={{
                 transform: `translateX(-${26}px)`,
-                transition: 'transform 0.5s ease',
+                transition: "transform 0.5s ease",
               }}
             >
               {featuredImages.map((image, index) => (
-                <div className='featured-image' key={index}>
+                <div className="featured-image" key={index}>
                   <img src={image} alt={`Project ${index + 1}`} />
-                   {/* Icon on top of the image */}
-                   <div className="icon-overlay">
+                  {/* Icon on top of the image */}
+                  <div className="icon-overlay">
                     <MdArrowOutward size={30} color="white" />
                   </div>
                 </div>
               ))}
             </div>
           </div>
-          <button className='hover-button' aria-label='See all projects'>
+          <button className="hover-button" aria-label="See all projects">
             <span>See All Projects</span>
           </button>
-        </Container>
+        </div>
       </div>
     </>
   );
